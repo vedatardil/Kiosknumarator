fd125c741500763114e4da5b82f0a2a0ada5a0a1
Vedat Ardil <46025440+vedatardil@users.noreply.github.com>
2018-12-20 18:33:17 +0300

    Kiosk Numaratör Uygulaması
    C# ile geliştirilmiş basit bir arayüze sahip termal yazıcıdan sıra numarası verebilen program.

---
+++ Numarator/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Numarator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(71, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(846, 251);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sıra Al";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(112, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(773, 107);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Göz Ölçüm Sırası";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Numarator.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(345, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 296);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T.C. SAĞLIK BAKANLIĞI TORBALI DEVLET HASTAHANESİ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Numarator/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace Numarator
+{
+    public partial class Form1 : Form
+    {
+        // PrintDocument yazdir = new PrintDocument();
+        const string txt_SiraAliniyor = "Sıra Alınıyor...";
+        const string txt_SiraAl = "Sıra Al";
+
+        int eni = 600;
+        int boyu = 600;
+        int sayac = 0;
+        //int sure = 0;
+        string NumaraKlasoru = null;
+        string NumaraDosyasi = null;
+        //string UpdateDosyası = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            printDocument1.DefaultPageSettings.PaperSize = new PaperSize("Barkod", eni, boyu);
+            printDocument1.PrinterSettings.PrinterName = "Brother HL-1110 series";
+            //yazdir.PrinterSettings.PrinterName = "Barkod";
+
+            timer1.Interval = 3000; // 2 sn
+            NumaraKlasoru = System.IO.Path.GetDirectoryName(Application.ExecutablePath.Replace("bin\\Debug", "").Replace("bin\\Release", "")) + @"\Data\";
+            NumaraDosyasi = NumaraKlasoru + "numaralar.txt";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button1.Text = txt_SiraAliniyor;
+                button1.Enabled = false;
+
+                // Klasör kontrol
+                if (!Directory.Exists(NumaraKlasoru))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(NumaraKlasoru);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(this, NumaraKlasoru + " yaratılamadı! Detaylar : " + ex.Message, "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        this.Close();
+                        return;
+                    }
+                }
+
+                // Txtkontrol
+                string veriler = null;
+
+                try
+                {
+                    if (!File.Exists(NumaraDosyasi))
+                    {
+                        veriler = string.Format("0;{0}", DateTime.Now.ToShortDateString());
+                        File.WriteAllText(NumaraDosyasi, veriler);
+                    }
+                    else
+                    {
+                        veriler = File.ReadAllText(NumaraDosyasi);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, NumaraDosyasi + " yaratılamadı! Detaylar : " + ex.Message, "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(veriler))
+                {
+                    MessageBox.Show(this, "Hiç Veri Yok!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                    return;
+                }
+
+                this.sayac = 0;
+                string okunanTarih = null;
+
+                try
+                {
+                    string[] temp = veriler.Split(';');
+                    sayac = int.Parse(temp[0]);
+                    okunanTarih = temp[1];
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, "Convert hatası! Detaylar : " + ex.Message, "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                    return;
+                }
+
+                string suan = DateTime.Now.ToShortDateString();
+                if (!okunanTarih.Equals(suan, StringComparison.Ordinal))
+                {
+                    sayac = 1;
+                    veriler = string.Format("{0};{1}", sayac, suan);
+                    File.WriteAllText(NumaraDosyasi, veriler);
+                }
+                else
+                {
+                    sayac++;
+                    veriler = string.Format("{0};{1}", sayac, suan);
+                    File.WriteAllText(NumaraDosyasi, veriler);
+                }
+
+                try
+                {
+                    printDocument1.Print();
+                }
+                catch
+                {
+                    MessageBox.Show("Yazcı ile ilgili bir sorun oluştu. Yazıcının açık olup olmadığını kontrol ediniz.", "Yazıcı Hatası");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bilinmeyen Bir hata oluştu! " + ex.Message, "Global Hata");
+            }
+            finally
+            {
+                timer1.Start();
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(DateTime.Now.ToLongDateString(), new Font("Arial", 10), new SolidBrush(Color.Black), new RectangleF(5, 5, this.eni, this.boyu));
+            e.Graphics.DrawString("Sıra Numaranız", new Font("Arial", 10), new SolidBrush(Color.Black), new RectangleF(20, 35, this.eni, this.boyu));
+            e.Graphics.DrawString(this.sayac.ToString(), new Font("Arial", 25), new SolidBrush(Color.Black), new RectangleF(40, 55, this.eni, this.boyu));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button1.Text = txt_SiraAl;
+            button1.Enabled = true;
+        }
+    }
+}
